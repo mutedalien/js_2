7a59b7882b258f5a480d33f290174465b88bccb7
Svyatoslav <chel_c@mail.ru>
2020-07-02 21:18:33 +0300

    less_6

---
+++ less_6/js/main.js
@@ -0,0 +1,20 @@
+const API = 'https://raw.githubusercontent.com/mutedalien/online-store-api/master/responses';
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        loadError: ''
+    },
+    methods: {
+        getJson(url){
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    this.loadError = error.stack;
+                })
+        },
+    },
+    mounted() {
+        console.log(this);
+    }
+});
